a1514271b064d70ef7abb18bc9e73e592fbb26ce
Andrew Quinlan <andrewquinlan22@gmail.com>
2022-02-27 19:21:23 -0500

    first commit

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const chalk  = require('chalk');
 
 var urlRoot = "https://api.github.com";
 // NCSU Enterprise endpoint:
-//var urlRoot = "https://api.github.ncsu.edu";
+// var urlRoot = "https://api.github.ncsu.edu";
 
-var userId = "yy2111";
+var userId = "doopydino";
 var config = {};
 // Retrieve our api token from the environment variables.
 config.token = process.env.GITHUBTOKEN;
@@ -25,10 +25,10 @@ if (process.env.NODE_ENV != 'test')
 {
 	(async () => {
 		await listAuthenicatedUserRepos();
-		//await listBranches(userId, "your repo");
-		//await createRepo(userId,"newrepo");
-		//await createIssue(userId, repo, issue);
-		//await enableWikiSupport(userId,repo);
+		await listBranches(userId, "REST");
+		// await createRepo(userId, "testRepo");
+		// await createIssue(userId, "REST", "testIssue", "testBody");
+		await enableWikiSupport(userId, "REST");
 
 	})()
 }
@@ -43,6 +43,7 @@ function getDefaultOptions(endpoint, method)
 			"content-type": "application/json",
 			"Authorization": `token ${config.token}`
 		}
+		
 	};
 	return options;
 }
@@ -94,12 +95,25 @@ function listAuthenicatedUserRepos()
 // 1. Write code for listBranches in a given repo under an owner. See list branches
 async function listBranches(owner,repo)
 {
-	let options = getDefaultOptions(`/`, "GET");
+	let options = getDefaultOptions(`/repos/${owner}/${repo}/branches`, "GET");
 
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
 	{
 		request(options, function (error, response, body) {
+			if( error )
+			{
+				console.log( chalk.red( error ));
+				reject(error);
+				return; // Terminate execution.
+			}
+
+			var obj = JSON.parse(body);
+			for( var i = 0; i < obj.length; i++ )
+			{
+				var name = obj[i].name;
+				console.log( name );
+			}
 
 			// console.debug( options );
 			resolve( JSON.parse(body) );
@@ -109,9 +123,12 @@ async function listBranches(owner,repo)
 }
 
 // 2. Write code to create a new repo
-async function createRepo(owner,repo)
+async function createRepo(owner, repo)
 {
-	let options = getDefaultOptions("/", "POST");
+	// let options = getDefaultOptions(`/${owner}/${repo}`, "POST");
+	let options = getDefaultOptions(`/orgs/StevensC2GS/repos`, "POST");
+	options.body = {"name":repo};
+	options.json = true;
 
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
@@ -125,9 +142,11 @@ async function createRepo(owner,repo)
 
 }
 // 3. Write code for creating an issue for an existing repo.
-async function createIssue(owner,repo, issueName, issueBody)
+async function createIssue(owner, repo, issueName, issueBody)
 {
-	let options = getDefaultOptions("/", "POST");
+	let options = getDefaultOptions(`/repos/${owner}/${repo}/issues`, "POST");
+	options.body = {"title":issueName, "body":issueBody};
+	options.json = true;
 
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
@@ -141,16 +160,19 @@ async function createIssue(owner,repo, issueName, issueBody)
 }
 
 // 4. Write code for editing a repo to enable wiki support.
-async function enableWikiSupport(owner,repo)
+async function enableWikiSupport(owner, repo)
 {
-	let options = getDefaultOptions("/", "PATCH");
+	let options = getDefaultOptions(`/repos/${owner}/${repo}`, "PATCH");
+	options.body = {"has_wiki":true};
+	options.json = true;
 
 	// Send a http request to url and specify a callback that will be called upon its return.
 	return new Promise(function(resolve, reject)
 	{
 		request(options, function (error, response, body) {
 
-			resolve( JSON.parse(body) );
+			// resolve( JSON.parse(body) );
+			resolve( response.statusCode );
 		});
 	});	
 }
